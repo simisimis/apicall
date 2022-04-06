72b7da4fa5f33a8eda17fe9635c7359397e315fc
Simonas Narbutas <snarbutas@bol.com>
2022-04-06 11:35:04 +0200

    make it print

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use serde::{Deserialize, Serialize};
 
-pub type Station = Vec<Datapoint>;
-
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Dadapoint {
@@ -18,8 +16,9 @@ pub struct Sensordatavalue {
 }
 
 fn main() {
+    let mut station = Vec::new();
     let json = r#"
-    [{
+    {
     "sensordatavalues": [
       {
         "value_type": "temperature",
@@ -35,9 +34,9 @@ fn main() {
       }
     ],
     "timestamp": "2022-04-06 08:42:38"
-    }]
+    }
     "#;
     let datapoint: Dadapoint = serde_json::from_str(json).unwrap();
-    let station: Station = (datapoint);
-    println!("{:?}", station);
+    station.push(datapoint);
+    println!("{:?}", station[0]);
 }
